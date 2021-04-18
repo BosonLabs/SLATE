4dcf73cebdfca3b6636c38ace1a9c5ffb785deaa
VaishnaviGnanasekaran <vaishnavignanasekaran610@gmail.com>
2021-04-18 20:07:15 +0530

    adoptioncommit

---
+++ WebApp/vaishnavi/adoption.sol
@@ -0,0 +1,28 @@
+//adopters adopt a pet by using petid (id from 0 to 15)
+//also return the address of adopter
+pragma solidity ^0.5.0;
+
+contract Adoption {
+    address[16] public adopters;
+
+    // Adopting a pet
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
+
+    // Retrieving the adopters
+    function getAdopters() public view returns (address[16] memory) {
+        return adopters;
+    }
+
+    // Clear all adopters
+    function clear() public {
+        for (uint i = 0; i < adopters.length; i++) {
+            adopters[i] = address(0);
+        }
+    }
+}
